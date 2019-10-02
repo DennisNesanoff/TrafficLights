320ef58833c164deda2ace73994d4c4ecded9e15
Dennis Nesanoff <nndennisnn@gmail.com>
2019-10-02 22:37:02 +0300

    Initial Commit

---
+++ TrafficLights/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrafficLights
+//
+//  Created by Dennis Nesanoff on 02/10/2019.
+//  Copyright © 2019 Dennis Nesanoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

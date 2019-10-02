7fabe62df201e1b0f6b4f6213d17e157f7aac73b
Dennis Nesanoff <nndennisnn@gmail.com>
2019-10-02 23:24:26 +0300

    Traffic work

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -7,14 +7,51 @@
 //
 
 import UIKit
+enum ColorOfTraffic {
+    case red, yellow, green, off
+}
 
 class ViewController: UIViewController {
-
+    @IBOutlet var redColor: UIView!
+    @IBOutlet var yellowColor: UIView!
+    @IBOutlet var greenColor: UIView!
+    @IBOutlet var button: UIButton!
+    
+    var traffic = ColorOfTraffic.off
+    let isOff: CGFloat = 0.3
+    let isOn: CGFloat = 1.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redColor.layer.cornerRadius = redColor.bounds.height / 2
+        yellowColor.layer.cornerRadius = yellowColor.bounds.height / 2
+        greenColor.layer.cornerRadius = greenColor.bounds.height / 2
+        
+        redColor.alpha = isOff
+        yellowColor.alpha = isOff
+        greenColor.alpha = isOff
     }
 
-
+    @IBAction func setColor(_ sender: UIButton) {
+        
+        switch traffic {
+        case .off:
+            redColor.alpha = isOn
+            traffic = .red
+        case .red:
+            redColor.alpha = isOff
+            yellowColor.alpha = isOn
+            traffic = .yellow
+        case .yellow:
+            yellowColor.alpha = isOff
+            greenColor.alpha = isOn
+            traffic = .green
+        case .green:
+            greenColor.alpha = isOff
+            redColor.alpha = isOn
+            traffic = .red
+        }
+    }
+    
 }
 
